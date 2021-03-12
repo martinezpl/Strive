5bd6f2276f6c9de970b489e1bae70e54ff4fdb14
Roy Fishermann <mszlesz@gmail.com>
2021-03-12 14:16:19 +0000

    disco filter

--- M6/03. Histograms, transformations and Bitwise ops/disco_filter.py
+++ M6/03. Histograms, transformations and Bitwise ops/disco_filter.py
@@ -11,24 +11,26 @@ if __name__ == "__main__":
     keyo = 255
 
     cv2.namedWindow("preview")
-
-
+    cv2.moveWindow("preview", 2000, 100)
+    
     cap = cv2.VideoCapture(0)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
-    size = (width, height)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('dance.avi', fourcc, 20.0, size)
+    # FOR RECORDING
+    #width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+    #height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+    #size = (width, height)
+    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    #out = cv2.VideoWriter('dance.avi', fourcc, 20.0, size)
+    
     if cap.isOpened():  # try to get the first frame
         rval, frame = cap.read()
     else:
         rval = False
-    cv2.moveWindow("preview", 2000, 100)
+    
     while rval:
         frame_f = disco_filter(frame, keyo)
         cv2.imshow("preview", frame_f)
         rval, frame = cap.read()
-        out.write(frame_f)
+        #out.write(frame_f)
         key = cv2.waitKey(20)
         if key == 27:  # exit on ESC
             break
@@ -38,5 +40,5 @@ if __name__ == "__main__":
         else:
             keyo = 255
     cap.release()
-    out.release()
+    #out.release()
     cv2.destroyWindow("preview")
